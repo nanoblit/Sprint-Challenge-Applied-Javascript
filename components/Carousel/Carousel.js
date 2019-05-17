9f17232d8970847660f3b2ba27bba5ba1af77c48
nanoblit <nanoblit@users.noreply.github.com>
2019-05-17 10:40:20 +0200

    Add auto scroll to the carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -3,16 +3,27 @@ class Carousel {
     this.element = element;
     this.images = this.element.querySelectorAll("img");
     this.currentIndex = 0;
+    this.timeout = null;
 
-    this.element
-      .querySelector(".left-button")
-      .addEventListener("click", () => this.changeImage(false));
+    this.element.querySelector(".left-button").addEventListener("click", () => {
+      clearTimeout(this.timeout);
+      this.changeImage(false);
+    });
 
     this.element
       .querySelector(".right-button")
-      .addEventListener("click", () => this.changeImage(true));
+      .addEventListener("click", () => {
+        this.changeImage(true);
+      });
+
+    this.element.addEventListener("mouseover", () => {
+      clearTimeout(this.timeout);
+      this.timeout = null;
+    });
+    this.element.addEventListener("mouseout", () => this.autoScroll());
 
     this.setCurrentImage();
+    this.autoScroll();
   }
 
   setCurrentImage() {
@@ -29,6 +40,16 @@ class Carousel {
     else if (this.currentIndex >= this.images.length) this.currentIndex = 0;
 
     this.setCurrentImage();
+  }
+
+  autoScroll() {
+    if (!this.timeout) {
+      this.timeout = setTimeout(() => {
+        this.timeout = null;
+        this.changeImage(true);
+        this.autoScroll();
+      }, 2000);
+    }
   }
 }
 
